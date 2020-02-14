1217e3758a624a3a9cd6aaba449889df85770787
Wladislaw <wladikowalew@gmail.com>
2020-02-14 12:22:23 +0300

    начали рефакторинг транспортного уровня

--- Geocoder/TransportLayer.m
+++ Geocoder/TransportLayer.m
@@ -10,12 +10,31 @@
 
 @implementation TransportLayer
 
-static NSString *const server = @"https://google.com";
-static NSString *t = @"https://geocode-maps.yandex.ru/1.x/?apikey=a880cf10-7c51-4526-9e32-517f6e45c777&geocode=london&format=json";
+static NSString *const SERVER = @"https://geocode-maps.yandex.ru/1.x/";
 
-+(NSData*) getQuery{
-    NSURL *url = [NSURL URLWithString:t];
-    NSLog(@"%@", t);
+static NSString *t = @"https://geocode-maps.yandex.ru/1.x/?apikey=&format=json&geocode=";
+
++(NSString *)getApiKey{
+    return @"a880cf10-7c51-4526-9e32-517f6e45c777";
+}
+
++(NSString *)getURLWithName:(NSString *)name withCount:(int) count{
+    NSMutableString *url = SERVER;
+    [url appendString:@"?"];
+    [url appendString:@"format=json&"];
+    [url appendFormat:@"apikey=%@&", [self getApiKey]];
+    [url appendFormat:@"results=%d&", count];
+    name = [name stringByAddingPercentEncodingWithAllowedCharacters:NSCharacterSet.URLHostAllowedCharacterSet];
+    [url appendFormat:@"geocode=%@", name];
+    return url;
+}
+
++(NSData *) getQuery{
+    NSString *name = @"Смоленск";
+    name = [name stringByAddingPercentEncodingWithAllowedCharacters:NSCharacterSet.URLHostAllowedCharacterSet];
+    NSString *q = [NSString stringWithFormat:@"%@%@",t,name];
+    NSURL *url = [NSURL URLWithString:q];
+    NSLog(@"%@", q);
     NSMutableURLRequest *request = [NSMutableURLRequest new];
     [request setHTTPMethod:@"GET"];
     [request setURL:url];
