f28431a0f52ea1e47151ae041e9a446cc7988c05
Wladislaw <wladikowalew@gmail.com>
2020-02-14 09:39:18 +0300

    Initial Commit

---
+++ Geocoder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Geocoder
+//
+//  Created by Student on 14/02/2020.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Geocoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Geocoder
+//
+//  Created by Student on 14/02/2020.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
